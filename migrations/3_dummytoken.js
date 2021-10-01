4ba2dd81497d4363d8e8af511ba23986b7f74dc1
“Fernando <fribas84@gmail.com>
2021-10-01 08:09:05 -0300

    added test, refactor deployments to tests

--- migrations/3_dummytoken.js
+++ migrations/3_dummytoken.js
@@ -3,14 +3,6 @@ const Dex = artifacts.require("Dex");
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(Token);
-  let dex = await Dex.deployed();
-  let token = await Token.deployed();
-
-  await dex.addToken(web3.utils.fromUtf8(token.symbol()),token.address);
-  await token.approve(dex.address,1000);
-  await dex.deposit(100, web3.utils.fromUtf8(token.symbol));
-  let balanceOfToken = await dex.balances(accounts[0], web3.utils.fromUtf8(token.symbol));
-
-  console.log(balanceOfToken);
+  
 
 };
